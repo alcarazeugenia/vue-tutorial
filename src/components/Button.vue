c7f40d02d7bfd16918ecda61aa20c379c8b121cf
Eugenia Alcaraz <ealcaraz@altimetrik.com>
2022-03-25 14:26:28 -0300

    CURD request made to json server

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+	<button @click="onClick()" :style="{ background: color }" class="btn">
+		{{ text }}
+	</button>
+</template>
+
+<script>
+export default {
+	name: "ButtonComponent",
+	props: {
+		text: {
+			type: String,
+			default: "Add",
+		},
+		color: {
+			type: String,
+			default: "green",
+		},
+	},
+	methods: {
+		onClick() {
+			this.$emit("btn-click");
+		},
+	},
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+	<header>
+		<h1>{{ title }}</h1>
+		<ButtonComponent
+			:text="showAddTask ? 'Close' : 'Add Task'"
+			:color="showAddTask ? 'red' : 'green'"
+			@btn-click="$emit('toggle-add-task')"
+		/>
+	</header>
+</template>
+
+<script>
+import ButtonComponent from "./Button.vue";
+export default {
+	name: "HeaderComponent",
+	props: {
+		title: {
+			type: String,
+			default: "Task Tracker",
+		},
+		showAddTask: Boolean,
+	},
+	components: {
+		ButtonComponent,
+	},
+};
+</script>
+
+<style scoped>
+header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 20px;
+}
+</style>
